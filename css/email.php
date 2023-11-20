9853dc9fba0fa4f001651b841b96ab41268a668c
Sachintes <138668814+Sachintes@users.noreply.github.com>
2023-11-20 16:26:11 +0530

    Add files via upload

---
+++ css/email.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $lease = $_POST["lease"];
+    $vacantProperties = $_POST["vacantProperties"];
+    $propertyLocation = $_POST["propertyLocation"];
+
+    $to = "sachintnm001@gmail.com"; // Replace with your Gmail address
+    $subject = "Property Lease Inquiry";
+
+    $message = "Name: $name\n";
+    $message .= "Email: $email\n";
+    $message .= "Phone No.: $phone\n";
+    $message .= "Lease Consideration: $lease\n";
+    $message .= "Vacant Properties: $vacantProperties\n";
+    $message .= "Property Location: $propertyLocation\n";
+
+    $headers = "From: $email";
+
+    // Set the content-type for sending HTML email
+    $headers .= "\r\n" . "Content-type: text/html; charset=UTF-8";
+
+    // Attempt to send the email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email. Please try again later.";
+    }
+}
+?>
