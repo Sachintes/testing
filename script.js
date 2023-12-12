c3fad638e0f85ce36cc541406710c88f3a3bf3de
Sachintes <138668814+Sachintes@users.noreply.github.com>
2023-12-12 11:58:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,12 @@
-"use strict";
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "sachintnm001@gmail.com",
+    Password : "DD5B22FFA888D5098594195243386849C9B2",
+    To : 'sachintnm001@gmail.com',
+    From : "sachintnm001@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
 
-const forms = document.forms;
-
-if (forms.length) {
-  for (const form of forms) {
-    const file = form.querySelector('input[type="file"]');
-    if (file) {
-      file.addEventListener('change', formAddFile);
-    }
-    form.addEventListener('submit', formSubmitAction);
-  }
-}
-
-function formAddFile(e) {
-  const formInputFile = e.target;
-  const formFiles = formInputFile.files;
-  const fileName = formFiles.length ? formFiles[0].name : '';
-  formInputFile.parentElement.nextElementSibling.innerHTML = fileName;
-}
-
-async function formSubmitAction(e) {
-  e.preventDefault();
-  const form = e.target;
-  const formAction = form.getAttribute('action') ? form.getAttribute('action').trim() : "#";
-  const formMethod = form.getAttribute('method') ? form.getAttribute('method').trim() : "GET";
-  const formData = new FormData(form);
-
-  form.classList.add('form-sending');
-
-  try {
-    const response = await fetch(formAction, {
-      method: formMethod,
-      body: formData
-    });
-
-    if (response.ok) {
-      alert('Form sent!');
-      form.classList.remove('form-sending');
-
-      const formInputFile = form.querySelector('input[type="file"]');
-      if (formInputFile) {
-        formInputFile.parentElement.nextElementSibling.innerHTML = '';
-      }
-      form.reset();
-    } else {
-      throw new Error('Form submission failed');
-    }
-  } catch (error) {
-    alert('Error: ' + error.message);
-    form.classList.remove('form-sending');
-  }
-}
